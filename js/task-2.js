ce15b2061641ed2d739f3a2f16c2da9016151111
JenyaLinx <sportpit2020@gmail.com>
2025-08-01 17:35:23 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,40 @@
+// function getShippingMessage(country, price, deliveryFee) {
+//     const totalPrices = price + deliveryFee
+//     return `Shipping to ${country} will cost ${totalPrices} credits`;
+// }
+
+// console.log(getShippingMessage("Australia", 120, 50));
+// console.log(getShippingMessage("Germany", 80, 20));
+// console.log(getShippingMessage("Sweden", 100, 20));
+
+// function formatMessage(message, maxLength) {
+//     if (message.length <= maxLength) {
+//       return message;
+//     } else {
+//       return message.slice(0, maxLength) + "...";
+//     }
+// }
+  
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+
+function makeArray(firstArray, secondArray, maxLength)
+{
+  const NewArray = firstArray.concat(secondArray);
+  if (NewArray.length > maxLength) {
+  return NewArray.slice(0, maxLength);
+  }
+  return NewArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
